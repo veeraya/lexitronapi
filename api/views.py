dcd45916105e5729a36d3c75b87e0c6e45bddc46
Veeraya Pupatwibul <veeraya.pupat@gmail.com>
2013-12-18 22:51:23 +0800

    take care of plural

--- api/views.py
+++ api/views.py
@@ -1,10 +1,27 @@
 from django.http import HttpResponse
 from api.models import Entry
-from django.shortcuts import render
+import simplejson
+import re
 
 def search(request, search_term):
     entries = Entry.objects.filter(esearch=search_term)
-    definition = ""
+    closest_search_term = ""
+    if len(entries) == 0:
+        p1 = re.compile('([a-zA-Z]+?)(es\Z)')
+        p2 = re.compile('([a-zA-Z]+?)(ss\Z)')
+        p3 = re.compile('([a-zA-Z]+?)(s\Z)')
+        if len(p1.findall(search_term)) > 0:
+            closest_search_term = p1.findall(search_term)[0][0]
+            entries = Entry.objects.filter(esearch=closest_search_term)
+        elif len(p2.findall(search_term)) == 0 and  len(p3.findall(search_term)) > 0:
+            closest_search_term = p3.findall(search_term)[0][0]
+            entries = Entry.objects.filter(esearch=closest_search_term)
+
+    found = True if len(entries) > 0 else False
+    definitions = []
     for (counter, entry) in enumerate(entries):
-        definition += str(counter) + ". " + entry.tentry + " "
-    return HttpResponse("Definition: " + definition)
+        definitions.append(entry.tentry)
+
+    data = {"found" : found, "search_term" : search_term, "closest_search_term" : closest_search_term, "definitions" : definitions}
+
+    return HttpResponse(simplejson.dumps(data), content_type='application/json',)
